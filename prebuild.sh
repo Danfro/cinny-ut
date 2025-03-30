f3c417265ad61d9108b582817633514a060cdabb
Danfro <one@frostinfo.de>
2025-03-30 22:49:16 +0200

    bump nvm to current version

--- prebuild.sh
+++ prebuild.sh
@@ -71,7 +71,7 @@ apply_patches () {
 setup_node () {
   if [ ! -d $NVM_DIR ]; then
     echo "Setting up node $NODE_VERSION"
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
     nvm install $NODE_VERSION
   else
