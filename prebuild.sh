e1b5d691d9c1b8b22564e292c2bf83750f52e4d2
Danfro <one@frostinfo.de>
2025-04-03 22:33:23 +0200

    drop not needed NVM_DIR config line

--- prebuild.sh
+++ prebuild.sh
@@ -89,7 +89,6 @@ setup_node () {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
   fi
   echo "initialize nvm"
-  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 
